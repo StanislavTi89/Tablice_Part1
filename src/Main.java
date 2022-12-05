f4dd6312b4b51e20526b2e90dfcfcfe9df0fae55
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-05 11:02:38 +0300

    Start_Tab-lice_Part1(Массивы_Часть1)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,14 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Массивы_Часть1!");
+
+        //Задача 1
+        System.out.println("Задача 1");
+        int [ ] bigBox = {5, 6, 12};
+        bigBox [0] = 5;
+        int bigBox1 = bigBox[0];
+        System.out.println(bigBox1);
+        System.out.println(bigBox[1]);
+        System.out.println(bigBox[2]);
     }
 }
