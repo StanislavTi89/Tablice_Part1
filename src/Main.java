8d9f881f728a219f503189e107d90cc6c0e2e492
StanislavTi89 <Timoshenko311989@gmail.com>
2022-12-05 21:10:05 +0300

    Start_Tab-lice_Part1(Массивы_Часть1)

--- src/Main.java
+++ src/Main.java
@@ -10,5 +10,33 @@ public class Main {
         System.out.println(bigBox1);
         System.out.println(bigBox[1]);
         System.out.println(bigBox[2]);
+
+        //Задача 2
+        System.out.println("Задача 2");
+        int [ ] oneMas = new int [1];
+        oneMas [0] = 1;
+        int oneMas1 = oneMas [0];
+        System.out.println(oneMas1);
+
+        int [ ] twoMas = new int [2];
+        twoMas [1] = 2;
+        int twoMas1 = twoMas [1];
+        System.out.println(twoMas1);
+
+        int [ ] freeMas = new int[3];
+        freeMas[2] = 3;
+        int freeMass1 = freeMas [2];
+        System.out.println(freeMass1);
+
+        //Задача 3
+        System.out.println("Задача 3");
+        double [] kilo = {1.57, 7.654, 9.986};
+        kilo [0] = 1.57;
+        double kilo1 = kilo[0];
+        System.out.println(kilo1);
+        System.out.println(kilo[1]);
+        System.out.println(kilo[2]);
+
+
     }
 }
